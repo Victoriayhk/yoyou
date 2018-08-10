a2e66f16aa78b7f29d26a3216d2553738ed4573a
yexuechao <675314520@qq.com>
2018-08-10 19:46:18 +0800

    [v1.0][PHPEmail]

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -11,14 +11,11 @@ class Index
     {
         $res = \think\Db::table('t_user')->select();
 	//var_dump($res);
-	$data[0] = ['name'=>'yxc', 'url'=>'www.hahaha.com'];
-	$data[1] = ['name'=>'yxc2', 'url'=>'wwwss'];
-	$res = ['data'=>$data, 'code'=>0, 'message'=>'very good'];
-	return json($res);
-    }
-    public function get_name()
-    {       
-        $str = '{"data":[{"name":"yxc","url":"www.hahaha.com"},{"name":"yxc2","url":"wwwss"}],"code":0,"message":"very good"}';
+    	$data[0] = ['name'=>'yxc', 'url'=>'www.hahaha.com'];
+    	$data[1] = ['name'=>'yxc2', 'url'=>'wwwss'];
+    	$res = ['data'=>$data, 'code'=>0, 'message'=>'very good'];
+
+       $str = '{"data":[{"name":"yxc","url":"www.hahaha.com"},{"name":"yxc2","url":"wwwss"}],"code":0,"message":"very good"}';
         $res2 = json_decode($str, true);
         var_dump($res2);
         echo "code : ";
@@ -27,11 +24,48 @@ class Index
         echo $res2["data"][0]["name"];
         echo "\n";
         echo $res2["data"][0]["url"];
-
+    	return json($res);
+    }
+    public function get_name()
+    {       
+ 
+        
     }
 
     public function is_registered(){
+        $arr = json_decode($_GET['data'], true);
+        $res = \think\Db::table('t_user')->where('vuid', $arr["vuid"]);
+        if ($res != null)
+        {
+            $retjson["errno"] = 0;
+            $retjson["data"]["is_registered"] = 1;
+            $retjson["data"]["user_id"] = $res["user_id"];
+            $retjson["data"]["user_name"] = $res["user_name"];
+            $retjson["data"]["email"] = $res["email"];
+        }
+        else
+        {
+            $retjson["errno"] = 0;
+            $retjson["data"]["is_registered"] = 0;
+        }
+        return json($retjson);
 
     }
-    
+    public function get_verification_code()
+    {
+        $arr = json_decode($_GET['data'], true);
+        $email = $arr["email"];
+        // 发邮件操作
+
+        $retjson["errno"] = 0;
+        return json($retjson);
+    }
+
+    public function check_
 }
+
+
+
+
+
+

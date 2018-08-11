3d11f2ff156e2f4d43b1ec013eb63748689977e4
yexuechao <675314520@qq.com>
2018-08-11 20:47:45 +0800

    python backend code added.

---
+++ backend_python/mail_scan.py
@@ -0,0 +1,241 @@
+#coding: utf-8
+
+"""
+扫描数据库内的mail脚本, 并:
+
+1. 对正在途中的mail的状态进行更新
+2. 对到达指定寄达时间的mail进行发送并更新状态
+"""
+
+import MySQLdb
+import json
+import datetime
+import time
+import sched
+import logging
+import logging.handlers
+
+import settings
+from send_email import EmailHandler
+
+# 发送信的最小时间单位粒度, 比如最小粒度是天, 那么系统每隔一天扫描一次未发送的信, 
+# 到了送信时间就送出去
+# 单位: 秒
+MINIMUM_TIME_GRANULARITY = 10
+
+DB_config = {
+    "host": "127.0.0.1",
+    "user": "root",
+    "passwd": "",
+    "db": "manji",
+    "charset": "utf8"
+}
+
+DB_attri = {
+    "table_name": ["t_mail", "t_mail_state"],
+    "t_mail": ["mail_id", "user_id", "address_id", "pub_time", "is_read", "arrive_time", "mail_content", "email"],
+    "t_mail_state": [ "mstate_id", "poster_id", "start_time", "end_time", "description", "mood", "mood_time", "mail_id"],
+}
+
+# create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('[%(asctime)s][%(levelname)s][%(name)s]: %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+
+# ==========================================================
+
+
+def connect_database():
+    """连接数据库"""
+    db = MySQLdb.connect(**DB_config)
+    cursor = db.cursor()
+    return db, cursor
+
+
+def disconnect_database(db):
+    """关闭数据库连接"""
+    db.close()
+
+
+def get_cur_time():
+    return int(round(time.mktime(datetime.datetime.now().timetuple())))
+
+
+def insert_a_fake_data(db, cursor):
+    # 用户
+    user_name = "julesyi"
+    vuid = "huikangy"
+    email = "huikangyi@gmail.com"
+    user_img = "www.baidu.com/img/bd_logo1.png"
+    sql_user = """INSERT INTO t_user(user_name, vuid, email, user_img) VALUES(\"{}\", \"{}\", \"{}\", \"{}\")""".format(user_name, vuid, email, user_img)
+    cursor.execute(sql_user)
+
+    # 地址
+    addr = "testShenzhen"
+    sql_address = """INSERT INTO t_address(addr) VALUES (\"{}\")""".format(addr)
+    cursor.execute(sql_address)
+
+    # 信使
+    poster_url = "www.baidu.com/img/bd_logo1.png"
+    poster_name = "pony"
+    poster_desc = "test 1分钟左右送达"
+    expect_time = 60
+    sql_poster = """INSERT INTO t_poster(poster_name, poster_url, poster_desc, expect_time) VALUES(\"{}\",\"{}\", \"{}\", {})""".format(poster_name, poster_url, poster_desc, expect_time)
+    cursor.execute(sql_poster)
+
+    # 主信内容
+    user_id = 1
+    address_id = 1
+    pub_time = int(round(time.mktime(datetime.datetime.now().timetuple())))
+    is_read = 0
+    arrive_time = pub_time + 10
+    mail_content = "test <h2>hello world</h2>"
+    email = "452570607@qq.com"
+    sql_mail = """INSERT INTO t_mail(user_id, address_id, pub_time, is_read, arrive_time, mail_content, email)
+    VALUES({}, {}, {}, {}, {}, \"{}\", \"{}\")""".format(user_id, address_id, pub_time, is_read, arrive_time, mail_content, email)
+    cursor.execute(sql_mail)
+
+    # 信的状态 1
+    mail_id = 1
+    start_time = pub_time
+    end_time = pub_time + 4
+    # mood = "test: I'm in a mood"
+    # mood_time = start_time + 60 * 10000
+    description = "信使遇到了暴雨, 可能会迟到"
+    sql_mail_state_1 = """INSERT INTO t_mail_state(mail_id, start_time, end_time, description) VALUES({}, {}, {}, \"{}\")""".format(mail_id, start_time, end_time, description)
+    cursor.execute(sql_mail_state_1)
+
+    # 信的状态 2
+    mail_id = 1
+    start_time = end_time + 1
+    end_time = arrive_time
+    # mood = "test: I'm in a mood"
+    # mood_time = start_time + 60 * 10000
+    description = "信使遇到了初恋, 跑起来就像一阵风"
+    sql_mail_state_2 = """INSERT INTO t_mail_state(mail_id, start_time, end_time, description) VALUES({}, {}, {}, \"{}\")""".format(mail_id, start_time, end_time, description)
+    cursor.execute(sql_mail_state_2)
+
+    # db.commit()
+    return 0
+
+
+def update_test_data(db, cursor):
+    # 信的到达时间
+    pub_time = int(round(time.mktime(datetime.datetime.now().timetuple())))
+    arrive_time = pub_time + 10
+    cursor.execute("""UPDATE t_mail SET pub_time={}, arrive_time={} WHERE mail_id={}""".format(pub_time, arrive_time, 2))
+
+    # 信的状态 1
+    start_time = pub_time
+    end_time = pub_time + 4
+    cursor.execute("""UPDATE t_mail_state SET start_time={}, end_time={} WHERE mstate_id={}""".format(start_time, end_time, 3))
+
+    # 信的状态 2
+    start_time = end_time + 1
+    end_time = arrive_time
+    cursor.execute("""UPDATE t_mail_state SET start_time={}, end_time={} WHERE mstate_id={}""".format(start_time, end_time, 4))
+
+    db.commit()
+    return 0
+
+
+def get_unsend_mails(cursor, cur_time):
+    """获取所有未发送的mail"""
+    # cur_time = int(round(time.mktime(datetime.datetime.now().timetuple())))
+    sql = """SELECT t_mail.mail_id as mail_id, t_mail.arrive_time as arrive_time FROM t_mail WHERE {} < t_mail.arrive_time""".format(cur_time)
+    cursor.execute(sql)
+    mails = cursor.fetchall()
+    return mails
+
+
+def form_email_content(mail_content, mail_states):
+    text = ""
+    html = mail_content
+    image_url_list = []
+    return text, html, image_url_list
+
+
+def parse_mail(cursor, mail_id):
+    # 获取mail信息
+    sql = """SELECT * FROM t_mail WHERE mail_id={}""".format(mail_id)
+    cursor.execute(sql)
+    mail = cursor.fetchall()[0]
+    print mail
+
+    # 获取mail的状态(们), 并按时间顺序排序
+    sql = """SELECT * FROM t_mail_state WHERE mail_id={} ORDER BY field(start_time) DESC""".format(mail_id)
+    cursor.execute(sql)
+    mail_states = cursor.fetchall()
+    print mail_states
+
+    email_to = [mail[5]]
+    subject = settings.standard_email_subject
+    text, html, image_url_list = form_email_content(mail[6], mail_states)
+    return email_to, subject, text, html, image_url_list
+
+
+def update_unsend_mails(cursor, mails, cur_time):
+    num_send_out = 0
+
+    """更新未发送mail的状态"""
+    for mail in mails:
+        mail_id = mail[0]
+        arrive_time = mail[1]
+        if (arrive_time + MINIMUM_TIME_GRANULARITY >= cur_time):
+            """到了需要送信的时间"""
+            email_to, subject, text, html, image_url_list = parse_mail(cursor, mail_id)
+            # mail寄达
+            try:
+                email_sender = EmailHandler(
+                    settings.smtp_server, settings.smtp_username,
+                    settings.smtp.password)
+                email_sender.send_email(
+                    settings.smtp_username, email_to,
+                    subject, text,
+                    html, image_url_list)
+
+                num_send_out += 1
+            except Exception as e:
+                logging.error("{} send mail failed. mail_id={}, mail_to=[{}]".format(
+                    str(e), mail_id, ", ".join(email_to)))
+
+            logging.info("sending out {} mail(s).".format(num_send_out))
+
+
+def test():
+    db, cursor = connect_database()
+    logger.info("connect to database succeed.")
+
+    cur_time = get_cur_time()
+    mails = get_unsend_mails(cursor, cur_time)
+    print cur_time, mails
+
+    cur_time = 1533922511
+    mails = get_unsend_mails(cursor, cur_time)
+    print get_cur_time(), mails
+    update_unsend_mails(cursor, mails, cur_time)
+
+    disconnect_database(db)
+    logger.info("disconnect database")
+
+
+# def main():
+#     schedule = sched.scheduler(time.time, time.sleep)
+#     schedule.enter(MINIMUM_TIME_GRANULARITY, 0, mail_maintain, None)
+#     schedule.run()
+
+
+if __name__ == "__main__":
+    test()
---
+++ backend_python/send_email.py
@@ -0,0 +1,173 @@
+#coding:utf-8
+
+import os
+import sys
+import time
+import smtplib  # 加载smtplib模块
+from email.mime.text import MIMEText
+from email.utils import formataddr
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+
+import logging
+
+
+def send_email_easy(sender, receiver, mail_subject, mail_content):
+    """发送邮件简单版, 不能发图片
+    1. 示例
+        sender = {
+            "server": "smtp.qq.com",
+            "server_port": 465,
+            "username": "452570607@qq.com",
+            "address": "452570607@qq.com",
+            "password": "xxxxxx"
+        }
+        receiver = {
+            "address": "julesyi@163.com"
+        }
+        mail_subject = "test"
+        mail_content = "hello, content"
+        send_email(sender, receiver, mail_subject, mail_content)
+    """
+    server = smtplib.SMTP_SSL(
+            sender["server"], sender["server_port"])
+
+    try:
+        server.login(sender["username"], sender["password"])
+        # logging.debug("login succeed.")
+    except:
+        logging.error("email server login error.")
+        return False
+
+    try:
+        msg = MIMEText(mail_content,'html','utf-8') 
+        msg['Subject'] = mail_subject
+        # server.sendmail(sender["address"], receiver["address"], msg.as_string()) 
+        # logging.debug("sending email succeed.")
+    except:
+        logging.error("email server sending message error. mail_subject={}".format(
+                mail_subject))
+        return False
+    
+    server.close()
+    return True
+
+
+class EmailHandler(object):
+    """发送邮件句柄
+
+    来源: https://blog.csdn.net/max229max/article/details/70923172
+    """
+    def __init__(self, smtpserver, user, pwd):
+        self.smtp = smtplib.SMTP()
+        self.smtpserver = smtpserver
+        self.smtpuser = user
+        self.smtppwd = pwd
+
+    def generateAlternativeEmailMsgRoot(self, strFrom, listTo, listCc, strSubject, strMsgText, strMsgHtml, listImagePath):
+        """导入邮件信息
+        """
+        # 邮件头部信息
+        msgRoot = MIMEMultipart('related')
+        msgRoot['Subject'] = strSubject
+        msgRoot['From'] = strFrom
+        msgRoot['To'] = ",".join(listTo)
+        if listCc:
+            msgRoot['Cc'] = ",".join(listCc)
+        # msgRoot.preamble = 'This is a multi-part message in MIME format.'
+
+        # 激活html格式邮件正文
+        msgAlternative = MIMEMultipart('alternative')
+        msgRoot.attach(msgAlternative)
+
+        msgContent = strMsgText.replace("\n","<br>") if strMsgText else ""
+        msgContent += "<br>" + strMsgHtml if strMsgHtml else "" 
+
+        # 引入图片
+        if listImagePath and len(listImagePath)>0:
+            msgHtmlImg = msgContent + "<br>"
+            for imgcount in range(0, len(listImagePath)):
+                msgHtmlImg += '<img src="cid:image{count}"><br>'.format(count=imgcount)
+            msgText = MIMEText(msgHtmlImg, 'html')
+            msgAlternative.attach(msgText)
+
+            # This example assumes the image is in the current directory
+            for i,imgpath in enumerate(listImagePath):
+                assert(os.path.exists(imgpath))
+                fp = open(imgpath, 'rb')
+                msgImage = MIMEImage(fp.read())
+                fp.close()
+
+                # Define the image's ID as referenced above
+                msgImage.add_header('Content-ID', '<image{count}>'.format(count=i))
+                msgRoot.attach(msgImage)
+        else:
+            msgText = MIMEText(msgContent, 'html')
+            msgAlternative.attach(msgText)
+
+        return msgRoot
+
+    def send_email(self, strFrom, listTo, strSubject, strMsgText, strMsgHtml=None, listImagePath=None, listCc=None):
+        """发送邮件"""
+        msgRoot = self.generateAlternativeEmailMsgRoot(strFrom, listTo, listCc, strSubject, strMsgText, strMsgHtml, listImagePath)
+
+        try:
+            self.smtp = smtplib.SMTP()
+            self.smtp.connect(self.smtpserver)
+            self.smtp.login(self.smtpuser, self.smtppwd)
+            if listCc:
+                listTo = listTo + listCc
+            self.smtp.sendmail(strFrom, listTo, msgRoot.as_string())
+            self.smtp.quit()
+            logger.info("Send mail success {0}".format(strSubject))
+        except Exception as e:
+            logging.error("ERROR:Send mail failed {0} with {1}".format(strSubject, str(e)))
+
+
+
+
+def test():
+    # send_email_easy() 使用
+    # sender = {
+    #     "server": "smtp.qq.com",
+    #     "server_port": 465,
+    #     "username": "452570607@qq.com",
+    #     "address": "452570607@qq.com",
+    #     "password": ""
+    # }
+    # receiver = {
+    #     "address": "julesyi@163.com"
+    # }
+    # mail_subject = "test"
+    # mail_content = "hello, content"
+    # send_email(sender, receiver, mail_subject, mail_content)
+
+    # EmailHandler使用
+    smtpserver = "smtp.qq.com"
+    smtpport = 465
+    username = "4525706070@qq.com"
+    password = ""
+    strFrom = '4525706070@qq.com'
+    strTo = ['julesyi@163.com']
+    strCc = []
+    strSubject = 'test email - text with image'
+    eh = EmailHandler(smtpserver, username, password)
+    imgpath = "test.png"
+    imgpath2 = "test2.png"
+    # eh.send_email(strFrom,strTo,"text mail","Hi it's Max, this is a test maill-----1","<h2>test html content</h2>")
+    eh.send_email(strFrom,strTo,"image mail","Hi it's Max,\n this is a test maill-----2","<h2>test html content</h2>", [imgpath,imgpath2], listCc=strCc)
+    # eh.send_email(strFrom,strTo,"image mail","Hi it's Max, this is a test maill-----2",listImagePath=[imgpath])
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG,
+        format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+        filemode='w+')
+    test()
+
+
+
+
+
+
+
